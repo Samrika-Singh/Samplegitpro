9b4b30779a9ad421c6d2c4d529fdbabcd038a62c
SamrikaSingh <samrika666@gmail.com>
2021-03-17 18:53:24 +0530

    my first commit

---
+++ src/main/java/SamrikaS/App.java
@@ -0,0 +1,18 @@
+package SamrikaS;
+
+import org.junit.Test;
+
+public class App {
+
+	@Test
+	public static void main(String[] args) {
+		
+		{
+				System.out.println("hiiiii JAva World");
+					
+
+		}
+
+}
+
+}
